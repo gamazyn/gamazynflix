102719ec0d102bec342b3c0f017a09c9b8ce9b55
Thiago Moreira <gamazyn@live.com>
2020-07-30 18:16:41 -0300

    Ajustes na URL do JSON

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -25,7 +25,9 @@ function CadastroCategoria() {
   };
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://gamazynflix.herokuapp.com/categorias';
     fetch(URL).then(async (response) => {
       const res = await response.json();
       setCategorias([...res]);
